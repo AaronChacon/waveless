8fa29f969fa92a4afa0582d3a73c5c1e468181b6
Aaron Chacon <aaronch.dev@gmail.com>
2025-04-17 01:05:11 +0200

    feat: footer

---
+++ src/app/pages/home-page/components/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+  imports: [],
+})
+export class MainComponent {}
--- src/app/pages/home-page/home-page.imports.ts
+++ src/app/pages/home-page/home-page.imports.ts
@@ -2,5 +2,12 @@ import { FooterComponent } from '../../components/ui-waveless/organisms/footer/f
 import { NavbarComponent } from '../../components/ui-waveless/organisms/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeroComponent } from './components/hero/hero.component';
+import { MainComponent } from './components/main/main.component';
 
-export const HomePageImports = [NavbarComponent, FooterComponent, HttpClientModule, HeroComponent];
+export const HomePageImports = [
+  HttpClientModule,
+  NavbarComponent,
+  HeroComponent,
+  MainComponent,
+  FooterComponent,
+];
